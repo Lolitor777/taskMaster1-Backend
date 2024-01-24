166e81495d33db6647e11093d5d11e805b21ca02
Victor <carvajal57685@gmail.com>
2024-01-24 09:41:33 -0500

    Se añadieron las variables de entorno para el despliegue

--- database/db.js
+++ database/db.js
@@ -1,8 +1,12 @@
 import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv'
+dotenv.config()
 
-const db = new Sequelize('taskmaster', 'root', '', {
-    host: 'localhost',
+
+const db = new Sequelize(`${process.env.DB_DATABASE}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+    host: `${process.env.DB_HOST}`,
     dialect: 'mysql'
 })
 
-export default db;
+export default db; 
+
